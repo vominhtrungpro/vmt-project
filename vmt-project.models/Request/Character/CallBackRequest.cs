5c9b41341310edfb66e931e27757b2359bd830f9
binh.phan@smsdome.com <vominhtrungpro@gmail.com>
2024-06-17 12:33:14 +0700

    update request

--- vmt-project.models/Request/Character/CallBackRequest.cs
+++ vmt-project.models/Request/Character/CallBackRequest.cs
@@ -8,14 +8,14 @@ namespace vmt_project.models.Request.Character
 {
     public class CallBackRequest
     {
-        public string id { get; set; }
-        public string type { get; set; }
-        public string messageid { get; set; }
-        public string status { get; set; }
-        public string from { get; set; }
-        public string to { get; set; }
-        public string message_type { get; set; }
-        public string message_text { get; set; }
-        public string message_media { get; set; }
+        public string? id { get; set; }
+        public string? type { get; set; }
+        public string? messageid { get; set; }
+        public string? status { get; set; }
+        public string? from { get; set; }
+        public string? to { get; set; }
+        public string? message_type { get; set; }
+        public string? message_text { get; set; }
+        public string? message_media { get; set; }
     }
 }
